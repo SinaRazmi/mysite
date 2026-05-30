9874bbfaccec7f0a24156a85d0afc9454e231c87
Sina_Razmi <sinarazmi94@gmail.com>
2026-05-30 21:57:28 +0330

    Add Custom Tags & Filter

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_view, name = 'blog'),
     path('<int:pid>', blog_single, name = 'single'),
-    # path('post-<int:pid>', test, name = 'test'),
+    path('test', test, name = 'test')
     
 ]
