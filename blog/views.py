a23d0f21741fb1765971640261a31d95385dd8a9
Sina_Razmi <sinarazmi94@gmail.com>
2026-06-03 23:38:48 +0330

    View the posts base on category

--- blog/views.py
+++ blog/views.py
@@ -3,18 +3,26 @@ from blog.models import Post
 
 # Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(status = 1)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
 
 def blog_single(request, pid):
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(status = 1)
     # post = get_object_or_404(posts, pk=pid, status=1)
-    post = get_object_or_404(posts, pk=pid)
+    post = get_object_or_404(posts, pk = pid)
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context)
 
 
 def test(request):
     return render(request, 'test.html')
+
+
+
+def blog_category(request, cat_name):
+    posts = Post.objects.filter(status = 1)
+    posts = posts.filter(category__name = cat_name)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
